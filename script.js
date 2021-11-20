1098aad140423f12bc24ce1f449d595bc00c2c62
Fima <fima1fim@hotmail.com>
2021-11-20 17:41:52 +0200

    Practice web

---
+++ script.js
@@ -0,0 +1,37 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navlinks = document.querySelectorAll('.nav-links li');
+
+
+
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navlinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+
+navSlide();
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-50px";
+  }
+}
